8f168560cfe39d56b5494fa93b2ca3860ae55df7
ilhamizaki <72953685+ilhamizaki@users.noreply.github.com>
2020-10-16 07:59:11 +0700

    fix biodata

---
+++ src/IF110119006Latihan1.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 
+ * NAMA     : Ilham zaki
+ * Kelas    : IF1
+ * NIM      : 10119006
+ * Deskripsi Program : Menampilkan Value Ke Layar
+ */
+public class IF110119006Latihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Latihan 1. Perintah menampilkan value ke layar");
+        System.out.println("BIODATA" + "\n" + "-------------------");
+        System.out.println("NAMA\t\t: Ilham Zaki");
+        System.out.println("ALAMAT\t\t: Jalan Nusa Indah Raya");
+        System.out.println("TEMPAT LAHIR\t: Gresik");
+        System.out.println("TANGGAL LAHIR\t: 18 Mei 2000");
+        System.out.println("-------------------");
+        System.out.println("HOBI\t\t: Design");
+        System.out.println("MAKES\t\t: Ayam Goreng");
+        System.out.println("MIKES\t\t: Susu");
+    }
+    
+}
